bd3f6236c1155bf271d7474c7c14d54396b934c9
narendra106 <126778971+narendra106@users.noreply.github.com>
2025-07-22 20:33:51 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -8,13 +8,3 @@ function hidebox() {
     sidemenu.style.display = 'none'
     sidemenu.style.left = -270
 }
-function showresume() {
-    const resume = document.querySelector('.resume')
-    resume.style.display = 'flex'
-    const sidemenu = document.querySelector('.sidemenu')
-    sidemenu.style.display = 'none'
-}
-function hideresume() {
-    const resume = document.querySelector('.resume')
-    resume.style.display = 'none'
-}
